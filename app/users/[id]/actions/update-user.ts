b76e7280e598ec5408cfedb5a9e6d730219c41a7
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-11-03 14:22:04 +0330

    Add redirect function to the app

--- app/users/[id]/actions/update-user.ts
+++ app/users/[id]/actions/update-user.ts
@@ -1,10 +1,15 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation"
 import { updateUser } from "services/data/users";
 
 export const updateUserAction = async (userID: string, formData: FormData) => {
-    await updateUser(userID, formData.get("name") as string);
+    let newName = formData.get("name") as string;
+
+    await updateUser(userID, newName);
 
     revalidatePath(`/users/${userID}`);
+
+    redirect(`/users/${userID}?name=${newName}`);
 };
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -7,7 +7,7 @@ const UsersPage = async () => {
   return (
     <section className="flex flex-col gap-4 p-4">
       {users.map((user) => (
-        <Link key={user.id} className="bg-white p-2" href={`/users/${user.id}`}>
+        <Link key={user.id} className="bg-white p-2" href={`/users/${user.id}?name=${user.name}`}>
           <pre>ID: {user.id}</pre>
           <pre>NAME: {user.name}</pre>
         </Link>
